c3439db9f5a66eccaac789f61a33c6a91a80e4b0
Miislandii <2015302580036@whu.edu.cn>
2017-11-10 10:13:52 +0800

    把距离运算提取到了静态类DistanceOperation中

---
+++ PRAlgorithmLibrary/ClusterAlgorithms/DistanceOperations.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRAlgorithmLibrary.ClusterAlgorithms
+{
+    static class DistanceOperations
+    {
+        public static decimal GetMinDistance(List<Vector> centers, Vector v, out int minDIndex)
+        {
+            decimal minD = decimal.MaxValue;
+            int index = -1;
+            for (int i = 0; i < centers.Count; i++)
+            {
+                decimal d = v.CalculateDistance(centers[i]);
+                if (d < minD) { minD = d; index = i; };
+            }
+            minDIndex = index;
+            return minD;
+        }
+
+
+        public static int GetMaxValueIndex(decimal[] minDs)
+        {
+            if (minDs.Length == 0) return -1;
+
+            int index = 0;
+            decimal maxValue = minDs[0];
+            for (int i = 1; i < minDs.Length; i++)
+            {
+                if (maxValue < minDs[i])
+                {
+                    index = i;
+                    maxValue = minDs[i];
+                }
+            }
+
+            return index;
+        }
+
+        public static decimal[] GetMinDs(List<Vector> centers, Vector[] vectors, out int[] indexes)
+        {
+            indexes = new int[vectors.Length];
+            decimal[] result = new decimal[vectors.Length];
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                result[i] = GetMinDistance(centers, vectors[i], out indexes[i]);
+            }
+
+            return result;
+        }
+
+        public static int GetMostDistantVectorIndex(Vector center, Vector[] vectors)
+        {
+            int index = -1;
+            decimal distance = 0;
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                if (vectors[i] == center) continue;
+
+                decimal di = vectors[i].CalculateDistance(center);
+
+                if (di > distance) { distance = di; index = i; }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ PRAlgorithmLibrary/ClusterAlgorithms/KMeansAlgorithm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static PRAlgorithmLibrary.ClusterAlgorithms.DistanceOperations;
+
+namespace PRAlgorithmLibrary.ClusterAlgorithms
+{
+    class KMeansAlgorithm : IClusterAlgorithm
+    {
+        public int K { get; set; }
+
+        public List<List<Vector>> Cluster(Vector[] vectors)
+        {
+            List<Vector> centers = new List<Vector>();
+
+            //任选K个初始聚类中心
+            for (int i = 0; i < K; i++)
+            {
+                centers.Add(vectors[i]);
+            }
+
+            //分配所有样本
+
+
+        }
+
+        public void SetParameter(string paraName, decimal paraValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
